cea056534986a4ecc10a619c9c3e92558ee9b6eb
Awwab <awwabsaleem09@gmail.com>
2025-11-27 17:36:09 +0530

    feat: add Students page and update routing in AppRoutes; enhance Sidebar with active link styles

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,15 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "../components/Layout";
 
+import Students from "../pages/students";
 import Overview from "../pages/overview";
 
-export default function AppRoutes() { 
-    return (
-        <Routes>
-            <Route path="/" element={ <Layout />} >
-                <Route index element={ <Overview />} />
-                {/* TODO- add students & reports route */}
-            </Route>
-        </Routes>
-    )
-}                                                          
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Overview />} />
+        <Route path="students" element={<Students />} />
+        {/* TODO- add students & reports route */}
+      </Route>
+    </Routes>
+  );
+}
